352c5c6f6a19ec41e0bb7c35c57751480587e302
kishore399 <kishore.j21.01@gmail.com>
2025-09-07 21:22:26 +0530

    feat: add home page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,14 +5,13 @@ const Header = () => {
 
   const handleClick = () => {
     navigate("/login");
-  }
+  };
 
   return (
     <>
       <nav className="fixed w-full top-0 left-0 z-50 bg-[#1E1E2E] bg-opacity-80 backdrop-blur-md transition-all duration-300 ease-in-out">
         <div className="max-w-7xl mx-auto px-4">
           <div className="flex justify-between items-center h-16">
-
             <div className="flex-shrink-0">
               <span className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600">
                 BookTrackr
@@ -20,14 +19,16 @@ const Header = () => {
             </div>
 
             <div className="flex items-center">
-              <button onClick={handleClick} className="px-6 py-2 rounded-full hover:border font-bold hover:bg-white hover:text-black transition-colors transform hover:-translate-y-1">
+              <button
+                onClick={handleClick}
+                className="px-6 py-2 rounded-full hover:border font-bold hover:bg-white hover:text-black transition-colors transform hover:-translate-y-1"
+              >
                 Login
               </button>
             </div>
           </div>
         </div>
       </nav>
-
     </>
   );
 };
